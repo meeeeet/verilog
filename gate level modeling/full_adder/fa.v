431fdb92ffb30a1fd6babd864921aa1e8a5c08b1
Meet Sangani <76646671+meeeeet@users.noreply.github.com>
2022-12-10 20:58:29 +0530

    Create fa.v

---
+++ gate level modeling/full_adder/fa.v
@@ -0,0 +1,14 @@
+module fa(sum,carry,a,b,c);
+	input a,b,c;
+	output sum,carry;
+	
+	wire x,y,z;
+	
+	xor a1(sum,a,b,c); //sum
+	and a2(x,a,b);
+	and a3(y,b,c);
+	and a4(z,c,a);
+	
+	or a5(carry,x,y,z);
+	
+endmodule
