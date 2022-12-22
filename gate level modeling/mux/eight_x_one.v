3139e823d33d12a4ba365faa60bd2f16c2295e48
Meet Sangani <76646671+meeeeet@users.noreply.github.com>
2022-12-22 02:38:06 -0800

    Update eight_x_one.v

--- gate level modeling/mux/eight_x_one.v
+++ gate level modeling/mux/eight_x_one.v
@@ -3,8 +3,8 @@ module eight_x_one(i,sel,y);
 	input [2:0]sel;
 	output y;
 	wire [1:0]xx;
-	four_x_one f1(i[3:0],sel[2:1],xx[0]);
-	four_x_one f2(i[7:4],sel[2:1],xx[1]);
-	two_x_one m1(xx[0],xx[1],sel[0],y);
+	four_x_one f1(i[3:0],sel[1:0],xx[0]);
+	four_x_one f2(i[7:4],sel[1:0],xx[1]);
+	two_x_one m1(xx[0],xx[1],sel[2],y);
 	
 endmodule
