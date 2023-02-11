588d7eecacddbf3fc519034db02d9a64bd06051a
Meet Sangani <LegendM3e86338meet@gmail.com>
2023-02-11 11:36:27 +0530

    for hardware testing

--- Lock/lock.v
+++ Lock/lock.v
@@ -12,17 +12,17 @@ module lock(
     cp
 );
 
-    input [3:0]digit_1,digit_2,digit_3,digit_4;
+    input digit_1,digit_2,digit_3,digit_4;
     input reset,clk;
     output out,buzzer;
 
-    wire [15:0]current_pass;
+    wire [3:0]current_pass;
     wire [2:0]wrong_attempt;
-    wire [15:0]pass_serial;
+    wire [3:0]pass_serial;
 
     //assign current_pass=16'b0001000100011010;
     output [2:0]count; //verification purpose
-    output [15:0]cp; //verification purpose
+    output [3:0]cp; //verification purpose
     assign count=wrong_attempt; //verification purpose
     assign cp=current_pass; //verification purpose
 
@@ -47,8 +47,8 @@ endmodule
 module compare(
     clk,pass_in,current_pass,reset,wrong_attempt,out
 );
-    input [15:0] pass_in;
-    input [15:0] current_pass;
+    input [3:0] pass_in;
+    input [3:0] current_pass;
     input clk,reset;
     output reg out;
     output reg [2:0]wrong_attempt;
@@ -82,13 +82,13 @@ module hextobin (
 
     input [3:0]digit_1,digit_2,digit_3,digit_4;
     input clk;
-    output reg [15:0]pass_serial;
+    output reg [3:0]pass_serial;
     always @(negedge clk ) begin
 
-        pass_serial[3:0]=digit_1;
-        pass_serial[7:4]=digit_2;
-        pass_serial[11:8]=digit_3;
-        pass_serial[15:12]=digit_4;
+        pass_serial[0]=digit_1;
+        pass_serial[1]=digit_2;
+        pass_serial[2]=digit_3;
+        pass_serial[3]=digit_4;
 
     end
 
@@ -102,9 +102,9 @@ module  update(
     clk,
     reset
 );
-    input [15:0]pass_serial;
+    input [3:0]pass_serial;
     input clk,reset;
-    output reg [15:0]current_pass;
+    output reg [3:0]current_pass;
 
     always @(negedge clk ) begin
         if (reset==1) begin
--- Lock/lock_tb.v
+++ Lock/lock_tb.v
@@ -4,14 +4,14 @@
 
 
 module lock_tb;
-    wire [3:0]digit_1,digit_2,digit_3,digit_4;
+    wire digit_1,digit_2,digit_3,digit_4;
     reg reset;
     reg clk;
     wire out;
     wire buzzer;
     wire [2:0]count;
-    wire [15:0]cp;
-    reg [15:0]digit;
+    wire [3:0]cp;
+    reg [3:0]digit;
 
     assign {digit_1,digit_2,digit_3,digit_4}=digit;
 
@@ -35,28 +35,28 @@ module lock_tb;
     end
 
     initial begin
-        digit=16'h ABCD;
+        digit=4'b0000;
         #8 reset=1;
         #3 reset=0;
-        #10 digit=16'h 10A1;
-        #10 digit=16'h 10A1;
-        #10 digit=16'h 10AF;
-        #10 digit=16'h 1CAF;
-        #10 digit=16'h ABCD;
-        #10 digit=16'h A0AF;
-        #10 digit=16'h 9999;
+        #10 digit=4'b 1011;
+        #10 digit=4'b 1111;
+        #10 digit=4'b 1101;
+        #10 digit=4'b 1100;
+        #10 digit=4'b 0000;
+        #10 digit=4'b 1010;
+        #10 digit=4'b 1111;
         #5 reset=1;
         #5 reset=0;
-        #10 digit=16'h 1234;
-        #10 digit=16'h 10AF;
-        #10 digit=16'h 19AF;
-        #10 digit=16'h 1AAF;
-        #10 digit=16'h AB1F;
-        #10 digit=16'h 9999;
-        #10 digit=16'h A9AA;
-        #10 digit=16'h A0AC;
-        #10 digit=16'h ADAA;
-        #10 digit=16'h A01A;
+        #10 digit=4'b 1011;
+        #10 digit=4'b 0001;
+        #10 digit=4'b 1010;
+        #10 digit=4'b 1101;
+        #10 digit=4'b 0010;
+        #10 digit=4'b 1111;
+        #10 digit=4'b 1010;
+        #10 digit=4'b 1110;
+        #10 digit=4'b 0011;
+        #10 digit=4'b 0000;
         #10 $finish;
 
     end
